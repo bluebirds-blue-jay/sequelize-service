64db711bc27a190e0bdf90f703aefbbb7144de16
Sylvain Estevez <sylvain@luxe.com>
2017-10-20 16:46:35 -0700

    :card_index: Fixed typings

--- src/interfaces/sequelize-service.ts
+++ src/interfaces/sequelize-service.ts
@@ -14,14 +14,14 @@ export interface ISequelizeService<A, CP> {
   getPrimaryKeyField(): keyof A;
   create(object: A): Promise<A>;
   createMany(objects: A[]): Promise<Collection<A>>;
-  find(filters: TFilters<A>, options?: TFindOptions<A, CP>): Promise<Collection<A>>;
+  find(filters: TFilters<A>, options?: TFindOptions<A, CP>): Promise<Collection<A & CP>>;
   warn(condition: boolean, message: string, data?: object): void;
-  findOne(filters: TFilters<A>, options?: TFindOneOptions<A, CP>): Promise<A>;
-  findByPrimaryKey(pk: string | number, options?: TFindByPrimaryKeyOptions<A, CP>): Promise<A>;
-  findByPrimaryKeys(pks: string[] | number[], options?: TFindByPrimaryKeyOptions<A, CP>): Promise<Collection<A>>;
+  findOne(filters: TFilters<A>, options?: TFindOneOptions<A, CP>): Promise<A & CP>;
+  findByPrimaryKey(pk: string | number, options?: TFindByPrimaryKeyOptions<A, CP>): Promise<A & CP>;
+  findByPrimaryKeys(pks: string[] | number[], options?: TFindByPrimaryKeyOptions<A, CP>): Promise<Collection<A & CP>>;
   update(filters: TFilters<A>, values: TValues<A>, options?: TUpdateOptions<A>): Promise<number>;
   updateByPrimaryKey(pk: string | number, values: TValues<A>, options?: TUpdateByPrimaryKeyOptions<A>): Promise<number>;
   delete(filters: TFilters<A>, options?: TDeleteOptions<A>): Promise<number>;
   count(filters: TFilters<A>, options?: TCountOptions<A>): Promise<number>;
-  replaceOne(filters: TFilters<A>, values: A, options?: TReplaceOneOptions<A, CP>): Promise<A>;
+  replaceOne(filters: TFilters<A>, values: A, options?: TReplaceOneOptions<A, CP>): Promise<A & CP>;
 }
