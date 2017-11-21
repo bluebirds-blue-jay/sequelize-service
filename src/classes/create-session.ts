f9c79467e6758796918a7245be46f91354585f3c
Nicolas Gramlich <nicolas.gramlich@volvocars.com>
2017-11-20 19:38:16 -0800

    :bug: Fixed remaining (now revealed) compile errors (around generics).

--- src/classes/create-session.ts
+++ src/classes/create-session.ts
@@ -4,7 +4,7 @@ import { ISequelizeService } from '../interfaces/sequelize-service';
 import { ICreateSession } from '../interfaces/create-session';
 
 export class CreateSession<W, R extends W, C> extends Session<W, R, C, TCreateOptions<R, C>> implements ICreateSession<W, R, C> {
-  public constructor(objects: R[], options: TCreateOptions<R, C>, service: ISequelizeService<W, R, C>) {
+  public constructor(objects: W[], options: TCreateOptions<R, C>, service: ISequelizeService<W, R, C>) {
     super(objects, options, service);
   }
 }
