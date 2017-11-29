c7d261880b51314cf12238f355cb5acc0bf72b04
Nicolas Gramlich <nicolas.gramlich@volvocars.com>
2017-11-29 15:48:37 -0800

    :zap: Added nullability to singular find signatures.

--- src/interfaces/sequelize-service.ts
+++ src/interfaces/sequelize-service.ts
@@ -17,12 +17,12 @@ export interface ISequelizeService<W extends {}, R extends W, C extends {}> {
   createMany<KR extends keyof R, KC extends keyof C = keyof {}>(objects: W[], options?: TCreateOptions<R, C, KC>): Promise<Collection<R & Pick<C, KC>>>;
   find<KR extends keyof R, KC extends keyof C = keyof {}>(filters: TFilters<R>, options?: TFindOptions<R, C, KR, KC>): Promise<Collection<Pick<R, KR> & Pick<C, KC>>>;
   warn(condition: boolean, message: string, data?: object): void;
-  findOne<KR extends keyof R, KC extends keyof C = keyof {}>(filters: TFilters<R>, options?: TFindOneOptions<R, C, KR, KC>): Promise<Pick<R, KR> & Pick<C, KC>>;
-  findByPrimaryKey<KR extends keyof R, KC extends keyof C = keyof {}>(pk: string | number, options?: TFindByPrimaryKeyOptions<R, C, KR, KC>): Promise<Pick<R, KR> & Pick<C, KC>>;
+  findOne<KR extends keyof R, KC extends keyof C = keyof {}>(filters: TFilters<R>, options?: TFindOneOptions<R, C, KR, KC>): Promise<(Pick<R, KR> & Pick<C, KC>) | null>;
+  findByPrimaryKey<KR extends keyof R, KC extends keyof C = keyof {}>(pk: string | number, options?: TFindByPrimaryKeyOptions<R, C, KR, KC>): Promise<(Pick<R, KR> & Pick<C, KC>) | null>;
   findByPrimaryKeys<KR extends keyof R, KC extends keyof C = keyof {}>(pks: string[] | number[], options?: TFindByPrimaryKeyOptions<R, C, KR, KC>): Promise<Collection<Pick<R, KR> & Pick<C, KC>>>;
   update(filters: TFilters<R>, values: TValues<W>, options?: TUpdateOptions<R>): Promise<number>;
   updateByPrimaryKey(pk: string | number, values: TValues<W>, options?: TUpdateByPrimaryKeyOptions<R>): Promise<number>;
   delete(filters: TFilters<R>, options?: TDeleteOptions<R>): Promise<number>;
   count(filters: TFilters<R>, options?: TCountOptions<R>): Promise<number>;
-  replaceOne<KR extends keyof R, KC extends keyof C = keyof {}>(filters: TFilters<R>, values: W, options?: TReplaceOneOptions<R, C, KC>): Promise<R & Pick<C, KC>>;
+  replaceOne<KC extends keyof C = keyof {}>(filters: TFilters<R>, values: W, options?: TReplaceOneOptions<R, C, KC>): Promise<R & Pick<C, KC>>;
 }
