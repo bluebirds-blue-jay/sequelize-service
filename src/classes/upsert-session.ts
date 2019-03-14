b3e05e81ba7d012a20a06db883c34ae9562848dc
Sylvain Estevez <sylvain@luxe.com>
2019-03-14 10:52:22 +0100

    :lock: Do not allow querying UpsertSession.created before it is set

--- src/classes/upsert-session.ts
+++ src/classes/upsert-session.ts
@@ -1,24 +1,31 @@
-import { Session } from './session';
-import { TUpsertOptions } from '../types/upsert-options';
 import { ISequelizeService } from '../interfaces/sequelize-service';
 import { IUpsertSession } from '../interfaces/upsert-session';
+import { TUpsertOptions } from '../types/upsert-options';
+import { Session } from './session';
 
 export class UpsertSession<W extends {}, R extends W, C extends {}, KC extends keyof C, O extends {} = {}> extends Session<W, R, C, TUpsertOptions<R, C, KC> & O> implements IUpsertSession<W, R, C, KC, O> {
-  private created: boolean | undefined = false;
+  private get created() {
+    if (this._created === undefined) {
+      throw new Error(`UpsertSession has not yet run.`);
+    }
+
+    return this._created;
+  }
+  private _created: boolean;
 
   public constructor(objects: W[], options: TUpsertOptions<R, C> & O, service: ISequelizeService<W, R, C>) {
     super(objects, options, service);
   }
 
   public setCreated(created: boolean): void {
-    this.created = created;
+    this._created = created;
   }
 
-  public isCreated(): boolean | undefined {
+  public isCreated(): boolean {
     return this.created;
   }
 
-  public isUpdated(): boolean | undefined {
-    return this.created === undefined ? undefined : !this.created;
+  public isUpdated(): boolean {
+    return !this.created;
   }
 }
